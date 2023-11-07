e175021e21d3e42db95d55f0aab9e9ca547fd243
Thilakh <thilakh2004@gmail.com>
2023-11-07 14:35:16 +0530

    First git

---
+++ src/Message.jsx
@@ -0,0 +1,9 @@
+function Message(){
+    return(
+        <div>
+            <h1 style={{backgroundColor:"blue",fontSize:"60px"}}>From Message.jsx</h1>
+            <p>Hello World</p>
+        </div>
+    );
+}
+export{Message};
