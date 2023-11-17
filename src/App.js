9f8a36ea0dc93eadff067dd5778162ded80f8440
Thilakh <thilakh2004@gmail.com>
2023-11-17 21:35:12 +0530

    render

--- src/App.js
+++ src/App.js
@@ -1,32 +1,15 @@
-/*import logo from "./logo.svg";
+import logo from "./logo.svg";
 import "./App.css";
-import { Message } from "./Message";
-import { List } from "./List";
-import Main from "./components/Main";
-import Footex from "./components/Footex";
-import Navbar from "./components/Navbar";*/
-import Hello from './day2/Hello';
-import Style from './day2/Style';
-import Button from './day3/Button';
-import {Goku} from './day3/Goku';
+import Counter from "./day3/Counter";
+import ToggleMessage from "./day3/ToggleMessage";
 
 function App() {
   return (
-   /* <div>
-     
-      <Navbar></Navbar>
-      <Main></Main>
-      <Footex></Footex>
-    </div>*/
-    <div className='flex space-x-4 my-4'>
-      <Hello></Hello>
-      <Button>Green</Button>
-      <Button>Red</Button>
-      <Button>Blue</Button>
-      <Button>Yellow</Button>
-      <Goku></Goku>
-      <Style></Style>
-    </div>
+    <div>
+       <h1>Toggle Message Example</h1>
+      <ToggleMessage />
+  <Counter />
+ </div>
   );
 }
 
