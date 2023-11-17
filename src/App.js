bd66c8bf7e4148f1211b4c38b41c573e0d62bb96
Thilakh <thilakh2004@gmail.com>
2023-11-17 09:45:43 +0530

    final

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { List } from "./List";
 import Main from "./components/Main";
 import Footex from "./components/Footex";
 import Navbar from "./components/Navbar";*/
+import Hello from './day2/Hello';
 import Button from './day3/Button';
 import {Goku} from './day3/Goku';
 
@@ -17,6 +18,7 @@ function App() {
       <Footex></Footex>
     </div>*/
     <div className='flex space-x-4 my-4'>
+      <Hello></Hello>
       <Button>Green</Button>
       <Button>Red</Button>
       <Button>Blue</Button>
--- src/day3/Goku.jsx
+++ src/day3/Goku.jsx
@@ -3,8 +3,8 @@ export const Goku = () => {
     const[isVisible,display]=useState("Kaioken");
     return(
         <div>
-            <input type='checkbox' onChange={()=>display(isVisible==="Kaioken"?"SuperSaiyan":"Kaioken")}/>
             <h1>{isVisible}</h1>
+            <input type='checkbox' onChange={()=>display(isVisible==="Kaioken"?"SuperSaiyan":"Kaioken")}/>
         </div>
     )
 }
